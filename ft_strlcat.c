2c1473fb04fe3263890e27b63b70617111752f83
Erika Maria Tavera Misas <etavera-@lab1r1s3.42abudhabi.ae>
2022-11-14 12:19:05 +0400

    libreria

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:12:05 by etavera-          #+#    #+#             */
-/*   Updated: 2022/11/14 10:18:17 by etavera-         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:19:24 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int	ft_strlcat(char *dest, char *src, int n)
 	j = 0;
 	while (dest[j] != '\0')
 	{
-				printf("%d", j);
+				// printf("%d", j);
 		j++;
 
 	}
 	while (src[i] != '\0' && i < n)
 	{
-		printf("%d", j + i);
+		// printf("%d", j + i);
 		// dest[j + i] = src[i];
 		i++;
 
@@ -41,13 +41,13 @@ int	main(void)
 {
 	char	dest[50];
 	char 	src[50];
-	strcpy(dest, "des");
-	strcpy(src, "fuente this is the source");
+	strcpy(dest, "123456789");
+	strcpy(src, "fuen");
 	printf("result %lu", strlen(dest));
 	printf("result %lu", strlen(src));
 	printf("%s", "-");
-	printf("%lu", strlcat(dest, src, 6));
+	printf("%lu", strlcat(dest, src, 10));
 	// printf("%s", "-");
-	// printf("%d", ft_strlcat(dest, src, 6));
+	printf("%d", ft_strlcat(dest, src, 10));
 	// puts(dest);
 }
