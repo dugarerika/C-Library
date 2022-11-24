5c88b927e1ad6ecb0fa53d45b6eb632d37af5d5b
Erika Maria Tavera Misas <etavera-@lab1r6s1.42abudhabi.ae>
2022-11-24 12:22:47 +0400

    strlcat fixes

--- ft_strlcat1.c
+++ ft_strlcat1.c
@@ -1,17 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 19:40:43 by etavera-          #+#    #+#             */
-/*   Updated: 2022/09/28 07:05:59 by etavera-         ###   ########.fr       */
+/*   Updated: 2022/11/24 12:22:17 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
 
 unsigned int	ft_length(char *s)
 {
@@ -19,32 +16,58 @@ unsigned int	ft_length(char *s)
 
 	k = 0 ;
 	while (s[k] != '\0')
-	{
 		k++;
-	}
 	return (k);
 }
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+size_t	ft_strlcat(char *dst, char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	l;
+	size_t	i;
+	size_t	ldst;
+	size_t	lsrc;
 
 	i = 0;
-	l = ft_length(dest);
-	while (src[i] != '\0' && i < size)
+	ldst = ft_length(dst);
+	lsrc = ft_length(src);
+
+	while (*dst && size > 0 && size --)
 	{
-		dest[l + i] = src[i];
+		dst ++;
 		i++;
 	}
-	dest[l + i] = '\0';
-	return (l + i +1);
+	while (*src && size > 1 && size --)
+	{
+		*dst++ = *src++;
+	}
+	if (size == 1)
+		*dst = '\0';
+	return (ldst + i);
 }
 
-// int	main(void)
-// {
-// 	char dest[] = "E dDF";
-// 	char src[] = "123456";
-// 	printf("%d", ft_strlcat(dest,src,3));
-// 	printf("%lu", strlcat(dest,src,3));
-// }
+int	main(void)
+{
+	char	primero[] = "Este es";
+	char	ultimo[] = "un potencial larga";
+	int	r;
+	int s;
+	int	tamano = 15;
+	char	buffer1[tamano];
+	char	buffer2[tamano];
+	strcpy(buffer1, primero);
+	strcpy(buffer2, primero);
+	r = strlcat(buffer1,ultimo,tamano);
+	s = ft_strlcat(buffer2,ultimo,tamano);
+	puts(buffer1);
+	puts(buffer2);
+	printf("Value returned: %d\n", r);
+	if (r > tamano)
+		puts("String truncated");
+	else
+		puts("String was fully copied");
+printf("Value returned: %d\n", s);
+	if (s > tamano)
+		puts("String truncated");
+	else
+		puts("String was fully copied");
+	return (0);
+}
